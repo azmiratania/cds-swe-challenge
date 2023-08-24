8d49ecdbff1c8002139177704e68ff05f161ecef
Azmira Tania <azmiratania@gmail.com>
2023-08-24 15:35:55 +0800

    Added fallbackCopyTextToClipboard

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -105,11 +105,54 @@ export default class HomePage extends Component {
         this.setState({ viewKey: e.target.value });
     }
 
-
+    /*
     // Main function to copy text to clipboard
     copyTextToClipboard(text) {
         if (!navigator.clipboard) {
             // fallbackCopyTextToClipboard(text);
+            return;
+        }
+        navigator.clipboard.writeText(text).then(
+            function () {
+                console.log('Async: Copying to clipboard was successful!');
+            }, function (err) {
+                console.error('Async: Could not copy text: ', err);
+            }
+        );
+    }
+    */
+
+    // Fallback function to copy text to clipboard
+    fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Fallback: Copying text command was ' + msg);
+        } catch (err) {
+            console.error('Fallback: Oops, unable to copy', err);
+        }
+
+        document.body.removeChild(textArea);
+    }
+
+
+
+    // Main function to copy text to clipboard
+    copyTextToClipboard(text) {
+        if (!navigator.clipboard) {
+            this.fallbackCopyTextToClipboard(text);
             return;
         }
         navigator.clipboard.writeText(text).then(
